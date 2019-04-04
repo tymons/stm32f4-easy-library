dc75c13ea910609e1566861986eca62c8db18be0
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2019-04-04 21:27:49 +0200

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "stm32f4xx.h"
+
+int main(void)
+{
+  int i = 0;
+
+  while (1)
+  {
+	i++;
+  }
+}
---
+++ src/stm32f4xx_it.c
@@ -0,0 +1,33 @@
+#include "stm32f4xx_it.h"
+
+/**
+  * @brief   This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+/*  TimingDelay_Decrement(); */
+}
+
